47083c62db850aab48a1be25293d6449f5002bb4
Haiyn <corexaren@gmail.com>
2020-03-22 17:21:27 +0100

    router implementation with index rerouting

--- src/index.php
+++ src/index.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Event Booking</title>
-</head>
-<body>
-    <?php
-        echo 'event-booking';
-    ?>
-</body>
-</html>
+<?php
+
+use components\core\Router;
+
+$router = new Router();
+
+$router->route([$_SERVER["REQUEST_URI"]]);
