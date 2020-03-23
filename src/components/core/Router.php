5f2d67a84bb3ce262aca958c1c073b45554b11e7
Haiyn <corexaren@gmail.com>
2020-03-23 18:25:30 +0100

    Added not found page and multiple word name support for router

--- src/components/core/Router.php
+++ src/components/core/Router.php
@@ -2,11 +2,25 @@
 
 namespace components\core;
 
-use controllers\RegisterController;
+use controllers\NotFoundController;
 
 class Router
 {
-    private function transformPathToController($path) {
+    /*
+     * Transforms the URL into a Controller name
+     */
+    private function _transformViewNameToController($viewName) {
+        // If the url has '-' in it, convert it to CamelCase
+        // e.g.: event-overview --> EventOverview
+        $parts = explode("-", $viewName);
+        foreach($parts as &$part)
+            $part = ucfirst(strtolower($part));
+        return implode("", $parts);
+    }
+
+    private function _transformPathToViewName($path) {
+        // Cut the argument after the host to size
+        // e.g. localhost:8080/event-overview?someparam --> event-overview
         $path = ltrim($path, "/");
         $path = trim($path);
         $path = explode("?", $path)[0];
@@ -15,27 +29,38 @@ class Router
         return $path[0];
     }
 
+    /*
+     * Routes from the URL to the correct Controller
+     */
     public function route($params) {
         $path = $params[0];
+        $viewName = $this->_transformPathToViewName($path);
+        $controllerName = $this->_transformViewNameToController($viewName);
 
-        $controllerName = $this->transformPathToController($path);
+        // This sets which Controller will be called if no path is given
         if (empty($controllerName))
-            $controllerName = "Register";
+            $controllerName = "EventOverview";
 
         $controllerClassName = $controllerName . "Controller";
 
+        // See if the called controller exists in the controllers folder
         if (file_exists("controllers/{$controllerClassName}.php")) {
+            echo "here";
             $className = "\\controllers\\"."$controllerClassName";
             $controller = new $className;
+
         }
         else {
-            $controller = new RegisterController();
-            // TODO: Handle this
+            // If not, use the NotFoundController
+            $controller = new NotFoundController();
+            $viewName = "not-found";
         }
 
-        $controller->viewName = strtolower($controllerName);
-        $controller->render($params);
+        // Set the viewname
+        $controller->viewName = $viewName;
 
+        // Invoke the controller view
+        $controller->render($params);
         $controller->showView();
     }
 }
--- src/controllers/Controller.php
+++ src/controllers/Controller.php
@@ -25,21 +25,4 @@ abstract class Controller
         extract((array)$this->view);
         require dirname(__DIR__)."/views/{$this->viewName}/{$this->viewName}.phtml";
     }
-
-
-
-    /**
-     * Load the model with the given name.
-     * loadModel("SongModel") would include models/songmodel.php and create the object in the controller, like this:
-     * $songs_model = $this->loadModel('SongsModel');
-     * Note that the model class name is written in "CamelCase", the model's filename is the same in lowercase letters
-     * @param string $model_name The name of the model
-     * @return object model
-     */
-    public function loadModel($model_name)
-    {
-        require '../models/' . $model_name . '.php';
-        // return new model (and pass the database connection to the model)
-        return new $model_name($this->db);
-    }
 }
