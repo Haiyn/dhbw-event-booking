a5aaede24fadcef606961111377d8dd3bcd57446
Merida <andrea.budimir.31@gmail.com>
2020-03-25 13:09:02 +0100

    header + footer

---
+++ src/views/shared/footer/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+            Event Booking DHBW  &copy2020
+                <a href="#">Impressum</a>
+                <a href="#">Datenschutz</a>
+    </div>
+    </nav>
+</footer>
+</body>
