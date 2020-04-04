5c2f352a572d7eb9a6b9ae185f4fad6c95335593
timo <timostroehlein@gmail.com>
2020-04-04 15:34:07 +0200

    added edit option to detail view added font-awesome to composer.json

--- src/controllers/EventDetailController.php
+++ src/controllers/EventDetailController.php
@@ -10,6 +10,8 @@ class EventDetailController extends Controller
 {
     public function render($params)
     {
+        session_start();
+        $_SESSION['USER_ID'] = "b89fe465-b4cf-4a3d-a006-19951bc31ae4";
         if (isset($_GET['event_id'])) {
             $event = Event::getInstance();
             $eventById = $event->getEventById(htmlspecialchars($_GET['event_id']));
@@ -17,9 +19,25 @@ class EventDetailController extends Controller
             $user = User::getInstance();
             if (isset($eventById->creator_id)) {
                 $creator = $user->getUserById($eventById->creator_id);
-            }
-            if (isset($creator)) {
-                $eventById->creator = $creator->username;
+
+                if (isset($creator)) {
+                    $eventById->creator = $creator->username;
+                }
+
+                $current_user_id = $_SESSION['USER_ID'];
+                $current_user = $user->getUserById($current_user_id);
+
+                if (isset($current_user) && isset($creator) && $current_user->user_id === $creator->user_id) {
+                    $this->view->isCreator = true;
+
+                    if (isset($_GET['edit'])) {
+                        $edit = $_GET['edit'];
+
+                        if ($edit) {
+                            $this->view->isReadonly = false;
+                        }
+                    }
+                }
             }
 
             $booking = Booking::getInstance();
@@ -30,5 +48,15 @@ class EventDetailController extends Controller
         }
 
         $this->view->pageTitle = "Event Details";
+
+        if (!isset($this->view->isCreator)) {
+            $this->view->isCreator = false;
+        }
+        if (!isset($this->view->isReadonly)) {
+            $this->view->isReadonly = true;
+        }
+
+        $this->view->isSuccess = isset($_GET["success"]);
+        $this->view->isError = isset($_GET["error"]);
     }
 }
