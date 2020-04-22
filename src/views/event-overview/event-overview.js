c8be09ed08f041998e3ec18b6958b54c7044cfab
timo <timostroehlein@gmail.com>
2020-04-22 20:00:53 +0200

    added comment so massi understands it

--- src/views/event-overview/event-overview.js
+++ src/views/event-overview/event-overview.js
@@ -15,11 +15,16 @@ $(document).ready(function () {
         filterDate();
     });
     $('#event-overview-search').on('keyup', function () {
+        // Get the search value and put it to lower case
         const value = $(this).val().toLowerCase();
+        // Filter all cards with the event-overview-filter class
         $('.event-overview-filter').filter(function () {
+            // Get the title and location of the card
             const title = this.querySelector('#event-overview-card-title');
             const location = this.querySelector('#event-overview-card-location');
             if (title != null) {
+                // Toggle the visibility of the card based on the match of the title/ location
+                // and the searched value
                 const toggle = $(title).text().toLowerCase().indexOf(value) > -1 ? true :
                     $(location).text().toLowerCase().indexOf(value) > -1;
                 $(this).toggle(toggle);
