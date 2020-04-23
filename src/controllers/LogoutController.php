599b9d26c30cd2cef8d2b1d0299193d3dab17035
Merida <andrea.budimir.31@gmail.com>
2020-04-23 02:35:26 +0200

    logout

--- src/controllers/LogoutController.php
+++ src/controllers/LogoutController.php
@@ -2,15 +2,21 @@
 
 namespace controllers;
 
+use models\User;
+
 class LogoutController extends Controller
 {
 
     public function render($params)
     {
+        $this->session->checkSession();
+
         if (isset($_POST['logout'])) {
             $this->session->unsetSession();
+            $this->redirect("login");
         }
 
     }
+
 }
 
