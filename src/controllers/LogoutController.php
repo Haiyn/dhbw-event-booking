b177740771aac5fa3772bbbec2bce96edacb1859
Haiyn <corexaren@gmail.com>
2020-04-24 21:09:33 +0200

    small fix

--- src/controllers/LogoutController.php
+++ src/controllers/LogoutController.php
@@ -2,16 +2,11 @@
 
 namespace controllers;
 
-use models\User;
-
 class LogoutController extends Controller
 {
-
     public function render($params)
     {
         $this->session->unsetSession();
-
     }
-
 }
 
